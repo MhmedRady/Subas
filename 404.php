57a48d94a8974803ee02a3db00d27da5a4c4ecdb
mhmedrady <mhmed.rady.95@gmail.com>
2021-02-06 12:35:18 +0200

    Up Data

---
+++ 404.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Error 404</title>
+  <link rel="icon" href="images\back\404.gif">
+  <link rel="stylesheet" href="layout\css\404.css">
+</head>
+<body style="background-image: url(images\back\404.gif);background-image: url(images/back/404.gif);
+  background-repeat: no-repeat;background-size: inherit;background-color: #1b1b1b;background-position: center top;">
+  <button>
+    <a href="index">Home Page</a>
+  </button>
+</body>
+</html>
---
+++ inc.php
@@ -0,0 +1,13 @@
+<?php
+
+$img = 'images/';
+
+$tpl   = 'includes/templates/';
+$css   = 'layout/css/';
+$js   = 'layout/js/';
+
+include $tpl  . 'Header.inc';
+
+include $tpl  . 'Navbar.php' ;
+
+ ?>
